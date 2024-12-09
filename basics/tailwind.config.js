0d0603217d254f67d89312d8ca2f405983eb11ca
Aravindakrishnan26 <aravindakrishnanbalaji@gmail.com>
2024-12-09 19:13:12 +0530

    changes made

---
+++ basics/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      korean: ['Noto Sans KR', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
